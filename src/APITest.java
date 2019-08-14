c3bbfa8abcb260f2197414c30dc0bbe0d5eec15b
Jon Horler <jonhorler@Ians-iMac.local>
2019-08-14 16:03:10 +0100

    Changed to allow for args. Jar file is now correct and the latest version

--- src/APITest.java
+++ src/APITest.java
@@ -8,14 +8,14 @@ public class APITest {
 	public static String sSoapRequest = "http://www.minkz.net/";
     public static void main(String[] args) throws Exception {
     	//String sCheck = "s";
-    	//sPdf = args[0];  //For Live
+    	sPdf = args[0];  //For Live
     	sApiAddress = "https://officemail.oursmart.co.uk/webservice/api.asmx";
     	if (System.getProperty("os.name").contains("Mac OS X"))
     	{
     		fl = System.getProperty("java.io.tmpdir") + sCustomerName + "/";
     		tmp = System.getProperty("user.home") + "/.MailADoc/GetTemplates.xml";
     		smp = System.getProperty("user.home") + "/.MailADoc/Sample.xml";
-    		sPdf = "/Users/jonhorler/Desktop/Test.pdf";
+    		//sPdf = "/Users/jonhorler/Desktop/Test.pdf";
     	}
     	else
     	{
