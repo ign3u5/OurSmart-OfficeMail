9db209b70dcfde20e4e5a72b9fe949adfae49d15
Jon Horler <jonhorler@Ians-iMac.local>
2019-08-14 09:09:40 +0100

    Added a show/hide option for passwords

--- src/Login.java
+++ src/Login.java
@@ -9,17 +9,18 @@ import java.io.IOException;
 
 public class Login implements ActionListener, ItemListener {
 
-	 	JFrame f=new JFrame("OurSmart OfficePrinter - Login");
+	 	JFrame fMain=new JFrame("OurSmart OfficePrinter - Login");
 
-	    JLabel l1=new JLabel("Username:");
-	    JLabel l2=new JLabel("Password:");
-	    JLabel l3=new JLabel("Remember Me:");
+	    JLabel lUsername=new JLabel("Username:");
+	    JLabel lPassword=new JLabel("Password:");
+	    JLabel lRemCred=new JLabel("Remember Me:");
 	    
-	    JTextField t1=new JTextField();
-	    JTextField t2=new JTextField();
-	    JCheckBox c1 = new JCheckBox();
+	    JTextField tUsername=new JTextField();
+	    JPasswordField pPassword=new JPasswordField();
+	    JCheckBox cRemCred = new JCheckBox();
+	    JCheckBox cShowPass = new JCheckBox();
 	    
-	    JButton b1=new JButton("Login");
+	    JButton bLogin=new JButton("Login");
         File fLoginXmlCheck = new File(APITest.fl + "Login.xml");
         Boolean bForgetCheck = false;
 	    
@@ -29,66 +30,76 @@ public class Login implements ActionListener, ItemListener {
 	    	if (!fTmpFolder.exists())
 	    		fTmpFolder.mkdir();
 	    	
-	        l1.setBounds(50,20,200,25);
-	        t1.setBounds(50,40,200,25);
-	        l2.setBounds(50,70,200,25);        
-	        t2.setBounds(50,90,200,25);
-	        //t2.setEchoChar('*');
-	        l3.setBounds(50, 120, 200, 25);
-	        c1.setBounds(150, 120, 25, 25);
-
+	    	//Set bounds
+	    	lUsername.setBounds(50,20,200,25);
+	    	lPassword.setBounds(50,70,200,25);
+	    	lRemCred.setBounds(50, 120, 200, 25);
+	    	tUsername.setBounds(50,40,200,25);
+	    	pPassword.setBounds(50,90,200,25);
+	    	cShowPass.setBounds(250, 90, 25, 25);
+	    	cRemCred.setBounds(150, 120, 25, 25);
+	    	bLogin.setBounds(50,150,200,20);
+	    	
 	        if (fLoginXmlCheck.exists()) {
-	        	t1.setText(XMLCommands.XmlAttributeRecall(APITest.fl + "Login.xml", "Username", ""));
-	        	t2.setText(XMLCommands.XmlAttributeRecall(APITest.fl + "Login.xml", "Password", ""));
-	        	t1.setEnabled(false);
-	        	t2.setEnabled(false);
-	        	c1.setSelected(true);
+	        	tUsername.setText(XMLCommands.XmlAttributeRecall(APITest.fl + "Login.xml", "Username", ""));
+	        	pPassword.setText(XMLCommands.XmlAttributeRecall(APITest.fl + "Login.xml", "Password", ""));
+	        	tUsername.setEnabled(false);
+	        	pPassword.setEnabled(false);
+	        	cShowPass.setEnabled(false);
+	        	cRemCred.setSelected(true);
 	        }
-	        b1.setBounds(50,150,200,20);
 	        
+	        pPassword.setEchoChar('*');
+	        cShowPass.setSelected(true);
 	       
-	        f.add(l1);
-	        f.add(l2);
+	        fMain.add(lUsername);
+	        fMain.add(lPassword);
 	        
-	        f.add(t1);
-	        f.add(t2);
+	        fMain.add(tUsername);
+	        fMain.add(pPassword);
 	        
-	        f.add(l3);
-	        f.add(c1);
+	        fMain.add(lRemCred);
+	        fMain.add(cRemCred);
+	        fMain.add(cShowPass);
 	        
-	        f.add(b1);
+	        fMain.add(bLogin);
 	        
 	        
-	        b1.addActionListener(this);
-	        c1.addItemListener(this);
-	        
-	        try
-	        {
-	        	f.setIconImage(ImageIO.read(getClass().getResourceAsStream("/Logo.png")));
-	        }
-	        catch (IOException ex)
-	        {
-	        	ErrorBox(ex.toString(), "Logo not found");
-	        }
-	        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        f.setLayout(null);
-	        f.setVisible(true);
-	        f.setSize(300,210);
+	        bLogin.addActionListener(this);
+	        cRemCred.addItemListener(this);
+	        cShowPass.addItemListener(this);
+	        fMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        fMain.setLayout(null);
+	        fMain.setVisible(true);
+	        fMain.setSize(300,210);
 
 	   }
 	    
 	    public void itemStateChanged(ItemEvent e) {
+	    	if(e.getSource() == cRemCred) {
 	    	if(e.getStateChange() == ItemEvent.DESELECTED) {
-	    		t1.setEnabled(true);
-	    		t2.setEnabled(true);
+	    		pPassword.setEnabled(true);
+	    		tUsername.setEnabled(true);
+	    		pPassword.setText("");
+	    		cShowPass.setEnabled(true);
 	    		bForgetCheck = true;
 	    	}
+	    	}
+	    	if (e.getSource() == cShowPass) {
+	    		if(e.getStateChange() == ItemEvent.SELECTED) {
+	    			pPassword.setEchoChar('*');
+	    		}
+	    		if (e.getStateChange() == ItemEvent.DESELECTED) {
+	    			pPassword.setEchoChar((char)0);
+	    		}
+	    	}
+
 	    }
 	    
 	    public void actionPerformed(ActionEvent e){
-	        if(e.getSource() == b1 ){
+	        if(e.getSource() == bLogin ){
 	        	String sCookieResult = "Error";
-	        	if ((t1.getText().isEmpty() || t2.getText().isEmpty()) && !fLoginXmlCheck.exists())
+	        	if ((tUsername.getText().isEmpty() || String.copyValueOf(pPassword.getPassword()).isEmpty()) && !fLoginXmlCheck.exists())
 	        	{
 	        		ErrorBox("Blank inputs remaining", "Blank Inputs");
 	        	}
@@ -101,8 +112,8 @@ public class Login implements ActionListener, ItemListener {
 			        		fLoginXmlCheck.delete();
 			        	}
 		        		if (!fLoginXmlCheck.exists())
-		        			XMLCommands.XmlLoginParser(APITest.smp, t1.getText(), t2.getText());
-		        		if (c1.isSelected())
+		        			XMLCommands.XmlLoginParser(APITest.smp, tUsername.getText(), String.copyValueOf(pPassword.getPassword()));
+		        		if (cRemCred.isSelected())
 		        		{
 		        			sCookieResult = SOAPCommands.LoginSoapRequest(APITest.sApiAddress, APITest.sSoapRequest + "Login", APITest.fl+ "Login.xml", false);
 		        		}
@@ -124,7 +135,7 @@ public class Login implements ActionListener, ItemListener {
 		            {
 		            	try {
 		            	SOAPCommands.APIRequest(APITest.sApiAddress, APITest.sSoapRequest + "GetTemplates", APITest.tmp, sCookieResult);
-		            	f.setVisible(false);
+		            	fMain.setVisible(false);
 		                new PostOptions(sCookieResult);
 		            	}
 		            	catch (Exception ex) {
