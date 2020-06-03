7c30111de9f23ee600a856741e1777d51725e22a
Chris <chrisfarmer19801@gmail.com>
2020-06-03 16:00:47 +0100

    Rebuilt jar file
    Replaced jar file in MailADoc_Portal_Install -> Program_Files for first time installs

--- src/APITest.java
+++ src/APITest.java
@@ -8,41 +8,46 @@ public class APITest {
 	public static String sSoapRequest = "http://www.minkz.net/";
     public static void main(String[] args) throws Exception {
     	//Set this to true if testing
-    	boolean testing = false;
+    	boolean testing = true;
     	
     	
     	InitializingProperties(args, testing);
     	new Login();
     }
     
+    private static void SetTestProperties()
+    {
+    	sApiAddress = "https://xeroxdemo.minkzmail.co.uk/webservice/api.asmx";
+    	if (System.getProperty("os.name").contains("Mac OS X"))
+			sPdf = "/Users/jonhorler/Desktop/Test.pdf"; //Directory on Mac
+    	else
+    		sPdf = "C:\\Users\\user\\Desktop\\Google.pdf"; //Directory on Windows
+    }
+    
+    private static void SetLiveProperties(String[] args)
+    {
+    	sApiAddress = "https://officemail.oursmart.co.uk/webservice/api.asmx";
+    	sPdf = args[0];
+    }
+    
     private static void InitializingProperties(String[] args, boolean testing)
     {
     	sCustomerName = "MailADoc";
     	if (testing)
-    	{
-    		sApiAddress = "https://xeroxdemo.minkzmail.co.uk/webservice/api.asmx";
-    	}
+    		SetTestProperties();
     	else
-    	{
-    		sPdf = args[0];
-    		sApiAddress = "https://officemail.oursmart.co.uk/webservice/api.asmx";
-    	}
+    		SetLiveProperties(args);
     	if (System.getProperty("os.name").contains("Mac OS X"))
     	{
     		fl = System.getProperty("java.io.tmpdir") + sCustomerName + "/";
     		tmp = System.getProperty("user.home") + "/.MailADoc/GetTemplates.xml";
     		smp = System.getProperty("user.home") + "/.MailADoc/Sample.xml";
-    		if (testing)
-    			sPdf = "/Users/jonhorler/Desktop/Test.pdf";
     	}
     	else
     	{
     		fl = System.getProperty("java.io.tmpdir") + sCustomerName + "\\";
     		smp = "other files\\Sample.xml";
     		tmp = "other files\\GetTemplates.xml";
-    		
-    		if (testing)
-    			sPdf = "C:\\Users\\user\\Desktop\\Google.pdf";
     	}
     }
 }
